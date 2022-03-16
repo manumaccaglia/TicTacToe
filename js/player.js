c61155ce737c56f42cdc4c52cb281f623915e923
manumaccaglia <manuelmaccaglia@gmail.com>
2022-03-16 11:35:20 -0300

    added player class

---
+++ js/player.js
@@ -0,0 +1,15 @@
+
+
+class player{
+    constructor(name, piece, color, score){
+        this.name = name;
+        this.piece = piece;
+        this.color = color;
+        this.score = score;
+    }
+}
+
+
+//Hay que crear los jugadores preguntando por prompt las preferencias y modificando el html en base a eso
+//TODO agregar jugadores en html
+const player1 = new player()
